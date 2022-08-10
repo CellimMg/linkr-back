57424d6bcc29cc7cc0fa64a8b0025580480a29c7
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-10 20:29:53 -0300

    adicionar dados para cada post

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -5,8 +5,10 @@ async function searchUsers(search){
 }
 async function userPost(userId){
     const {rows:userData} = await connection.query(`SELECT name,picture_url FROM users WHERE id = $1`,[userId])
-    const {rows:posts} = await connection.query(`SELECT * FROM posts
+    const {rows:posts} = await connection.query(`SELECT posts.id AS "postId", posts.link_url AS link, posts.description
+    FROM posts
     WHERE posts.user_id = $1`,[userId])
+
     const data = {
         name:userData[0].name,
         picture:userData[0].picture_url,
