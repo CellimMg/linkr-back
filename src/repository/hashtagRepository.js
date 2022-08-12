8a471132fd5ed0c7ddd3629b2552ef907b8874fd
HannahEbb <hannah.ebb.he@gmail.com>
2022-08-12 10:05:20 -0300

    db correction, hashtagRepository correction, added hashtagRouter to index

--- src/repository/hashtagRepository.js
+++ src/repository/hashtagRepository.js
@@ -3,13 +3,14 @@ import connection from "../dbStrategy/postgres.js";
 async function getPostsByHashtag(hashtag) {
     return connection.query(`SELECT 
     posts.id AS id, 
-    posts.user_id, 
-    users.name, 
-    users.picture_url, 
-    posts.link_url AS link, 
-    posts.description AS description 
+    posts.user_id AS "userId", 
+    users.name AS "username", 
+    users.picture_url AS "userImage", 
+    posts.link_url AS "link", 
+    posts.description AS "description" 
     FROM posts
-    JOIN hashtags ON posts.id = hashtags.post_id
+    JOIN "hashtags-posts" ON posts.id = "hashtags-posts".post_id
+	JOIN hashtags ON "hashtags-posts".hashtag_id = hashtags.id
     JOIN users ON posts.user_id = users.id
     WHERE hashtags.hashtag = $1`, [hashtag]);
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
 import timelineRoutes from './timelineRoutes.js';
 import userRoutes from './userRoutes.js';
+import hashtagRouter from './hashtagRoutes.js';
+
 const router = Router();
 
 router.use(timelineRoutes);
 router.use(userRoutes)
+router.use(hashtagRouter)
 
 export default router;
