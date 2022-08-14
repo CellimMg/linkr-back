7be9469606c60aff0e80f875be13ab35d44ca681
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-14 14:22:25 -0300

    [feature]validando token nas rotas

--- src/routes/likesRoutes.js
+++ src/routes/likesRoutes.js
@@ -2,12 +2,14 @@ import {Router} from 'express';
 import { postLike, getLikes,unLike, whoLikes } from '../controller/likesController.js';
 import { schemaValidate } from "../middlewares/schemaValidateMiddleware.js";
 import likeSchema from '../schemas/likeSchema.js';
+import { tokenValidation } from '../middlewares/authMiddleware.js';
+
 
 const likesRoute = Router();
 
-likesRoute.post('/like',schemaValidate(likeSchema),postLike)
-likesRoute.get('/like/:userId/:postId',getLikes)
-likesRoute.delete('/unlike',schemaValidate(likeSchema),unLike)
-likesRoute.get('/likes/:postId/:userId',whoLikes)
+likesRoute.post('/like',tokenValidation, schemaValidate(likeSchema),postLike)
+likesRoute.get('/like/:userId/:postId',tokenValidation, getLikes)
+likesRoute.post('/unlike',tokenValidation, schemaValidate(likeSchema),unLike) // deleta o like
+likesRoute.get('/likes/:postId/:userId',tokenValidation, whoLikes)
 
 export default likesRoute;
