782d4eeddb8b7b952271a6cd75789214e100033a
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-14 15:07:27 -0300

    [fix]ajustes de quantidade de likes

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -4,20 +4,17 @@ async function searchUsers(search){
    return await connection.query(`SELECT * FROM users WHERE users.name ILIKE '${search}%' `)
 }
 async function userPost(userId){
-    let quantyLikes = 0
     const {rows:userData} = await connection.query(`SELECT name,picture_url FROM users WHERE id = $1`,[userId])
     if(userData.length === 0){
         return (404)
     }
-    const {rows:likes} = await connection.query(`SELECT COUNT(*) FROM likes WHERE ${userId}= likes.user_id GROUP BY likes.id`)
-    if(likes.length >= 1){
-        quantyLikes = likes[0].count
-    }
     const {rows:posts} = await connection.query(`
-    SELECT posts.id AS "postId", posts.link_url AS link, posts.description, $1 AS likes
+    SELECT posts.id AS "postId", posts.link_url AS link, posts.description, COUNT(likes) AS likes
     FROM posts 
-    WHERE posts.user_id = $2
-    `,[quantyLikes,userId])
+    LEFT JOIN likes ON likes.post_id = posts.id
+    WHERE posts.user_id = $1
+    GROUP BY posts.id
+    `,[userId])
     
     const data = {
         name:userData[0].name,
