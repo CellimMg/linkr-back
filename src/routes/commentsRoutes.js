c7f65a2a087d13ffa79df853e5e1576bb1da68e0
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-17 14:07:07 -0300

    [fix]ajuste dados retornados pelas querys

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -1,5 +1,5 @@
 import {Router } from 'express';
-import { comment, listComments } from '../controller/commentsController.js';
+import { comment } from '../controller/commentsController.js';
 import { schemaValidate } from "../middlewares/schemaValidateMiddleware.js";
 import commentsSchema from '../schemas/commentsSchema.js';
 import { tokenValidation } from '../middlewares/authMiddleware.js';
@@ -7,7 +7,6 @@ import { tokenValidation } from '../middlewares/authMiddleware.js';
 
 const commentRoute = Router();
 
-commentRoute.get('/comments',tokenValidation,listComments);
 commentRoute.post('/comments',tokenValidation,schemaValidate(commentsSchema),comment)
 
 export default commentRoute;
