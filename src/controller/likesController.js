58858c3a46b8fedaf928f6cde48f680b9a3a70b9
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-16 21:18:49 -0300

    [fix]ajustes da quantidade de requisicao para os likes

--- src/controller/likesController.js
+++ src/controller/likesController.js
@@ -12,24 +12,6 @@ export async function postLike(req,res){
         res.sendStatus(500);
     }
 }
-export async function getLikes(req,res){
-    try{
-        const userId = req.params.userId;
-        const postId = req.params.postId
-    
-        const likes =await likesRepository.likes(userId,postId)
-        if(likes.rowCount === 1){
-            return res.send(true)
-        }else{
-            return res.send(false)
-        }
-        
-
-    }catch(error){
-        console.log(error);
-        res.sendStatus(500);
-    }
-}
 export async function unLike(req,res){
     try{
         const {userId,postId} = req.body;
@@ -43,24 +25,3 @@ export async function unLike(req,res){
     }
 }
 
-export async function whoLikes(req,res){
-    const postId = req.params.postId
-    const userId = parseInt(req.params.userId);
-    const resposta = {
-        names:[],
-        quantyLikes: 0
-    }
-    const likesNames = await likesRepository.likesNames(postId)
-    likesNames.rows.map((e)=> {
-        if(e.user_id === userId){
-            resposta.names.unshift('Você')
-        }else{
-            resposta.names.push(e.name)
-        }
-        
-        resposta.numero ++ 
-
-    })
-
-    res.send(resposta)
-}
--- src/routes/likesRoutes.js
+++ src/routes/likesRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { postLike, getLikes,unLike, whoLikes } from '../controller/likesController.js';
+import { postLike,unLike } from '../controller/likesController.js';
 import { schemaValidate } from "../middlewares/schemaValidateMiddleware.js";
 import likeSchema from '../schemas/likeSchema.js';
 import { tokenValidation } from '../middlewares/authMiddleware.js';
@@ -8,8 +8,6 @@ import { tokenValidation } from '../middlewares/authMiddleware.js';
 const likesRoute = Router();
 
 likesRoute.post('/like',tokenValidation, schemaValidate(likeSchema),postLike)
-likesRoute.get('/like/:userId/:postId',tokenValidation, getLikes)
 likesRoute.post('/unlike',tokenValidation, schemaValidate(likeSchema),unLike) // deleta o like
-likesRoute.get('/likes/:postId/:userId',tokenValidation, whoLikes)
 
 export default likesRoute;
