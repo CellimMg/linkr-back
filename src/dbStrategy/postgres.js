2de129e0fa9e7d463764d542c573f88169b62f89
Marcelo <cello.ma16@gmail.com>
2022-08-18 19:18:08 -0300

    [fix] ssl

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -6,7 +6,9 @@ const { Pool } = pg;
 
 const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
 
 const connection = new Pool(databaseConfig);
